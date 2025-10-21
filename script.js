14805872da8fc345437970acbd02b7ab780d6a5f
thongster <106802301+thongster@users.noreply.github.com>
2025-10-21 14:44:17 +0700

    added header and button

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 const myLibrary = [];
 
 const display = document.querySelector(".display");
+const newBook = document.querySelector(".newBook");
+const formSection = document.querySelector(".formsection");
+const bookForm = document.querySelector(".bookForm");
 
+let count = 2;
 
 // book object constructor
 function Book(title, author, pages, read) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
   }
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
+  this.Title = title;
+  this.Author = author;
+  this.Pages = pages;
+  this.Read = read;
   this.id = crypto.randomUUID();
   this.info = function() {
     return `${title} by ${author}, ${pages} pages, ${read}`;
@@ -41,7 +45,22 @@ function displayLibrary(myLibrary) {
     });
 };
 
+
+newBook.addEventListener("click", function() {
+    if (count % 2 === 1) {
+        bookForm.classList.add("bookShow")
+    };
+    
+    if (count % 2 === 0) {
+        bookForm.classList.remove("bookShow")
+    };    
+    count++;
+});
+
 addBookToLibrary("the war of art", "some guy", "100", "has been read");
 addBookToLibrary("All I Do Is Win", "T Pain", "12", "has not been read")
+addBookToLibrary("Atomic Habits", "James Clear", "320", "has been read");
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "310", "has been read");
+addBookToLibrary("The Subtle Art of Not Giving a F*ck", "Mark Manson", "224", "has been read");
 
 // displayLibrary(myLibrary)
